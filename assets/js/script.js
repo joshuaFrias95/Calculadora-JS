36eb6e1c1c350782016317652e80db9c5752ee38
joshuaFrias95 <metatron1495@gmail.com>
2023-04-30 01:59:46 -0600

    comienza lógica del programa

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const calculadora = document.querySelector("calc-body");
+// const botones = document.querySelector("buttons-row");
+const button = document.getElementsByClassName("fa-solid")
+
+if (button.onclick) {
+    console.log("button");
+}
